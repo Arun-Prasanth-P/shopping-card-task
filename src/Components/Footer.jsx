f14340bb167c86adec25f466e812f33923a0f412
Arun-Prasanth-P <arunmeenu1999@gmail.com>
2025-04-15 23:35:05 +0530

    small changes

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
       <footer className="py-5 bg-dark">
         <div className="container">
           <p className="m-0 text-center text-white">
-            Copyright © Your Website 2024
+          
           </p>
         </div>
       </footer>
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -8,7 +8,7 @@ const Header = () => {
           <div className="text-center text-white">
             <h1 className="display-4 fw-bolder">Shop in style</h1>
             <p className="lead fw-normal text-white-50 mb-0">
-              With this shop hompeage template
+         
             </p>
           </div>
         </div>
