c53577b5ed801aefc64379b90d4641e913e15ed8
Arun-Prasanth-P <arunmeenu1999@gmail.com>
2025-06-02 23:37:32 +0530

    corrections

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,51 @@
-/* eslint-disable react/prop-types */
-import'./App.css'
-const App=(props)=> {
+
+import PropTypes from "prop-types";
+import "./App.css";
+
+function App({ salelive, name, rating, price, pno, cartincrease, cartdecrease, available = false }) {
 
   return (
     <>
-          <div className="card card-name">
-             <div className="card-header" style={{textAlign:'center'}}>
-                <p style={{margin:10}}>{props.type}</p>
-               <h2 style={{ fontSize: 50,margin:1 }}>${props.value}/month</h2>
-             </div>
-         <div className="card-body "style={{paddingLeft:20}}>
-               <h6>✔    {props.features[0].name}</h6>
-               <h6>✔    {props.features[1].name}</h6>
-               <h6>✔    {props.features[2].name}</h6>
-               <h6>✔    {props.features[3].name}</h6>
-               {props.features[4].avaiable ? (<h6>✔     {props.features[4].name}</h6>) : (
-               <h6 className='textinto'>❌     {props.features[4].name}</h6>)}
-               {props.features[5].avaiable ? (<h6>✔     {props.features[5].name}</h6>) : (
-               <h6 className='textinto'>❌     {props.features[5].name}</h6>)}
-               {props.features[6].avaiable ? (<h6>✔     {props.features[6].name}</h6>) : (
-               <h6 className='textinto'>❌     {props.features[6].name}</h6>)}
-               {props.features[7].avaiable ? (<h6>✔     {props.features[7].name}</h6>) : (
-               <h6 className='textinto'>❌     {props.features[7].name}</h6>)}
-         </div>
-         <button type="button" style={{background:'blue',color:'white',borderRadius:120,width:300,height:50,margin:5}}>BUTTON</button>
+      <div className="card-deck" style={{ width: "18rem", display: "inline-block", margin: "40px 10px 50px 60px", textAlign: "center" }}>
+        <div className="card" style={{ height: "380px", position: "relative" }}>
+          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTVnNgxRSenFfN106Vs7mj98qhEKZyKgeISTQ&s" className="card-img-top" alt="..." />
+          {salelive ? (<h5 className="card-title card_tag" >sale</h5>) :
+            <></>}
+          <div className="card-body">
+            <h5 className="card-title">{name}</h5>
+            {rating ? (<h6 style={{ color: "green" }}><i className="fa fa-star" aria-hidden="true"></i>
+              <i className="fa fa-star" aria-hidden="true"></i>
+              <i className="fa fa-star" aria-hidden="true"></i>
+              <i className="fa fa-star" aria-hidden="true"></i>
+              <i className="fa fa-star" aria-hidden="true"></i></h6>) : <p></p>}
+            {price.pricechange ? (
+              <span style={{ textDecoration: "line-through" }}>{price.oldprice}</span>
+            ) : (
+              <h6></h6>
+            )} <span>  {price.newprice}</span>
+          </div>
+          {available ? (<button type="button" className="btn btn-outline-dark m-4" onClick={() => cartdecrease(pno)}>Remove from Cart</button>)
+            :
+            (<button type="button" className="btn btn-outline-dark m-4" onClick={() => cartincrease(pno)}>Add to Cart</button>)}
         </div>
+      </div>
     </>
   )
 }
+App.propTypes = {
+  salelive: PropTypes.string,
+  name: PropTypes.string,
+  rating: PropTypes.bool,
+  price: PropTypes.shape({
+    oldprice: PropTypes.string,
+    pricechange: PropTypes.bool,
+    newprice: PropTypes.string
+  }),
+  cartbutton: PropTypes.string,
+  cartincrease: PropTypes.func,
+  cartdecrease: PropTypes.func,
+  available: PropTypes.bool,
+  pno: PropTypes.string
+}
 
-export default App;
+export default App
